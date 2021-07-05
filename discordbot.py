13d7a5326bf22ba6e58c7541dea287b8ab270f89
ayucchiyukkuri <86958112+ayucchiyukkuri@users.noreply.github.com>
2021-07-05 19:37:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,12 +12,7 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
     
-@bot.event
- async def on_message(self, message):
-        if "" in message.content:
-            with open("log.txt", "a") as w:
-                w.write("[NAME]:" + message.author.name + " [MESSAGE]:" + message.content + "\n")
-            print("[NAME]:" + message.author.name + " [MESSAGE]:" + message.content)
+
 
 
 @bot.command()
